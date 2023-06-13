4569dd4facbaf0f3e691410868a04529aa9379a1
ThaliaBarrera <thalia.barrera@glovoapp.com>
2023-06-13 19:07:07 +0200

    Move setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="modern-data-stack-project",
+    packages=find_packages(),
+    install_requires=[
+        "dbt-core",
+        "dbt-bigquery",
+        "dagster",
+        "dagster-cloud",
+        "dagster-dbt",
+        "dagster-airbyte",
+    ],
+    extras_require={"dev": ["dagit", "pytest"]},
+)
